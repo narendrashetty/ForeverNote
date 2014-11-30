c545f07e81ad8cf7c983b7639fa08384e7bc6ae1
Narendra N Shetty <mailnarendra15@gmail.com>
2014-11-30 09:56:19 +0530

    implement basic pagination by adding a offset filter

--- app/controllers/notelistcontroller.js
+++ app/controllers/notelistcontroller.js
@@ -2,5 +2,39 @@ ForeverNote.controller('NoteListController', [
 	'$scope',
 	'NoteService',
 	function ($scope, NoteService) {
+		$scope.notesPerPage = 2;
+		$scope.currentPage = 0;
 		$scope.notes = NoteService.getAllNotes();
+		$scope.filtered = $scope.notes;		
+		$scope.prevPage = function() {
+			if ($scope.currentPage > 0) {
+				$scope.currentPage--;
+			}
+		};
+
+		$scope.prevPageDisabled = function() {
+			return $scope.currentPage === 0 ? "disabled" : "";
+		};
+
+		$scope.pageCount = function() {
+			return Math.ceil($scope.filtered.length/$scope.notesPerPage)-1;
+		};
+
+		$scope.nextPage = function() {
+			if ($scope.currentPage < $scope.pageCount()) {
+				$scope.currentPage++;
+			}
+		};
+
+		$scope.nextPageDisabled = function() {
+			return $scope.currentPage === $scope.pageCount() ? "disabled" : "";
+		};
+
+		$scope.setPage = function(n) {
+			$scope.currentPage = n;
+		};
+
+		$scope.$watch('query.title', function (val) {
+			$scope.currentPage = 0;
+		});
 	}]);
---
+++ app/filters/offsetfilter.js
@@ -0,0 +1,6 @@
+ForeverNote.filter('offset', function () {
+	return function(input, start) {
+		start = parseInt(start, 10);
+		return input.slice(start);
+	};
+});
